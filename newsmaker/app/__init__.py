52436686d8d98d449e09487cae098aed464cacc5
dima.vitvickiy <dimavitvickiy@gmail.com>
2018-10-23 07:20:35 +0300

    fixed flake8

--- newsmaker/app/__init__.py
+++ newsmaker/app/__init__.py
@@ -8,12 +8,13 @@ from redis import Redis, RedisError
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
+
 async def handle(request):
     try:
         visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Welcome, {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return web.Response(
