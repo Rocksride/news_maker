07a2f14f6a9f0661f5a1f33e1e4237999cf3a257
Rocksride <ihavenonameTaras@gmail.com>
2018-10-29 14:38:15 +0200

    installed ramda, js-cookie, wrote auth module

--- frontend/store/modules/auth.js
+++ frontend/store/modules/auth.js
@@ -1,22 +1,103 @@
-const state = {
+import * as R from 'ramda'
+import * as api from '@/api'
+// import * as helpers from '@/helpers'
+import Cookie from 'js-cookie'
+const log = R.tap(console.log);
 
+const state = {
+  token: null
 }
 
 const getters = {
-
+  token: state => state.token,
+  isAuthenticated: state => state.token != null
 }
 
 const mutations = {
-
+  clearToken: state => state.token = null,
+  setToken: (state, payload) => {
+    state.token = payload;
+  }
 }
 
 const actions = {
 
+  initAuth: ({ commit, dispatch }, req) => {
+    if (req) {
+      if (!req.headers.cookie) {
+        return;
+      }
+      const jwtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('jwt='));
+      if (!jwtCookie) {
+        return;
+      }
+      const token = jwtCookie.split('=')[1];
+      const expirationDate = req.headers.cookie.split(';').find(c => c.trim().startsWith('tokenExpiration=')).split('=')[1];
+
+      if (new Date().getTime() > +expirationDate || !expirationDate || !token) {
+        dispatch('logout');
+        return;
+      }
+      commit('setToken', token);
+    }
+    else if (process.client) {
+      const token = localStorage.getItem('token')
+      const expirationDate = localStorage.getItem('tokenExpiration');
+
+      if (new Date().getTime() > +expirationDate || !expirationDate || !token) {
+        dispatch('logout');
+        return;
+      }
+
+      commit('setToken', token);
+    }
+  },
+  signUp: async ({ commit }, payload) => {
+    try {
+      const resp = await api.signUp(payload)
+      const data = resp.data;
+
+      // commit('setToken', data.idToken);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  signIn: async ({ commit, dispatch }, payload) => {
+    try {
+      const resp = await api.signIn(payload)
+      const data = resp.data
+      commit('setToken', data.idToken);
+
+      const expirationDate = new Date().getTime() + Number.parseInt(data.expiresIn) * 1000
+
+      if (process.client) {
+        localStorage.setItem('token', data.idToken);
+        localStorage.setItem('tokenExpiration', expirationDate);
+      }
+
+      Cookie.set('jwt', data.idToken)
+      Cookie.set('tokenExpiration', expirationDate);
+
+      // return api.postApi({data: 'Authenticated message'})
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  logout: ({ commit, dispatch }) => {
+    commit('clearToken');
+    if (process.client) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('tokenExpiration')
+    }
+    Cookie.remove('jwt')
+    Cookie.remove('tokenExpiration');
+  }
+
 }
 
 export default {
-    state,
-    getters,
-    mutations,
-    actions
+  state,
+  getters,
+  mutations,
+  actions
 }
