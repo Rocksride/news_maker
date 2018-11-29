c69989b1858975ea358f5af5eef77f251bbf2c08
dima.vitvickiy <dimavitvickiy@gmail.com>
2018-11-29 22:09:00 +0200

    add lib files

---
+++ newsmaker/lib/auth.py
@@ -0,0 +1,37 @@
+import hashlib
+import logging
+
+import jwt
+from flask import current_app as app
+
+
+log = logging.getLogger()
+
+
+def generate_password_hash(password):
+    password_bin = password.encode()
+    password_sha256 = hashlib.sha256(password_bin)
+    return password_sha256.hexdigest()
+
+
+def create_jwt(user_data):
+    return jwt.encode(
+        {
+            'login': user_data['login'],
+            'password': generate_password_hash(user_data['password'])
+        },
+        app.config['SECRET'],
+        algorithm='HS256'
+    ).decode('utf-8')
+
+sdasd
+def decode_jwt(token):
+    try:
+        return jwt.decode(
+            token,
+            app.config['SECRET'],
+            algorithms=['HS256']
+        )
+    except jwt.exceptions.DecodeError as exc:
+        log.error(exc)
+        return None
---
+++ newsmaker/lib/helpers/views.py
@@ -0,0 +1,6 @@
+from flask import jsonify
+
+
+def error_response(errors, code=400):
+    return jsonify({'errors': errors}), code
+sd
