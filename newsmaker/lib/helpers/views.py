d77bf00783308565246ce73f457c7751605d16ad
dima.vitvickiy <dimavitvickiy@gmail.com>
2018-11-29 22:48:05 +0200

    add user_authorised helper

--- newsmaker/lib/helpers/views.py
+++ newsmaker/lib/helpers/views.py
@@ -1,5 +1,22 @@
-from flask import jsonify
+from functools import wraps
+
+from flask import jsonify, request, abort
+
+from newsmaker.bl.auth import validate_token
 
 
 def error_response(errors, code=400):
     return jsonify({'errors': errors}), code
+
+
+def user_authenticated(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token.startswith('Bearer'):
+            token = token[7:]
+        if not validate_token(token):
+            abort(401)
+        func(*args, **kwargs)
+    return wrapper
+
