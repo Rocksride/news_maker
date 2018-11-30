0525e4b7d757c216f62aed0090388e4cb9f1f3ec
dima.vitvickiy <dimavitvickiy@gmail.com>
2018-11-30 22:24:31 +0200

    add rubric creation

--- newsmaker/views/rubrics.py
+++ newsmaker/views/rubrics.py
@@ -1,16 +1,33 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask.views import MethodView
 from marshmallow import Schema, fields, validate
 
-from newsmaker.bl.rubrics import get_rubrics
+from newsmaker.bl.rubrics import get_rubrics, validate_rubric, save_rubric
+from newsmaker.lib.helpers.views import user_authorised, error_response
 
 
 class RubricSchema(Schema):
     id = fields.Integer(allow_none=True)
     title = fields.String(validate=[validate.Length(max=255)])
+    description = fields.String(
+        validate=[validate.Length(max=255)],
+        allow_none=True,
+        default=''
+    )
 
 
 class RubricView(MethodView):
     def get(self):
         rubrics = get_rubrics()
         return jsonify(RubricSchema().dump(rubrics, many=True).data)
+
+    @user_authorised
+    def post(self):
+        rubric_data, errors = RubricSchema().load(request.json)
+        if errors:
+            return error_response(errors)
+        errors = validate_rubric(rubric_data)
+        if errors:
+            return error_response(errors)
+        save_rubric(rubric_data)
+        return jsonify('success')
