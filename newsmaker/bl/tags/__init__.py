b30b44e6cf6ed9b92421ea3c3d3eefcd9d7daad1
dima.vitvickiy <dimavitvickiy@gmail.com>
2018-11-30 22:26:40 +0200

    add tag creation

--- newsmaker/bl/tags/__init__.py
+++ newsmaker/bl/tags/__init__.py
@@ -2,6 +2,23 @@ from newsmaker.models.tags import Tag
 from newsmaker.services.db import db
 
 
+def validate_tag(tag_data):
+    errors = {}
+    tag_title_already_exists = db.session.query(
+        Tag.query.filter(
+            Tag.title == tag_data.get('title')
+        ).exists()
+    ).scalar()
+    if tag_title_already_exists:
+        errors.update({"title": "Tag title already exists"})
+    return errors
+
+
+def save_tag(tag_data):
+    db.session.add(Tag(title=tag_data.get('title')))
+    db.session.commit()
+
+
 def get_tags():
     return db.session.query(
         Tag.id,
--- newsmaker/views/tags.py
+++ newsmaker/views/tags.py
@@ -1,8 +1,9 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask.views import MethodView
 from marshmallow import Schema, fields, validate
 
-from newsmaker.bl.tags import get_tags
+from newsmaker.bl.tags import get_tags, validate_tag, save_tag
+from newsmaker.lib.helpers.views import user_authorised, error_response
 
 
 class TagSchema(Schema):
@@ -14,3 +15,14 @@ class TagView(MethodView):
     def get(self):
         tags = get_tags()
         return jsonify(TagSchema().dump(tags, many=True).data)
+
+    @user_authorised
+    def post(self):
+        tag_data, errors = TagSchema().load(request.json)
+        if errors:
+            return error_response(errors)
+        errors = validate_tag(tag_data)
+        if errors:
+            return error_response(errors)
+        save_tag(tag_data)
+        return jsonify('success')
